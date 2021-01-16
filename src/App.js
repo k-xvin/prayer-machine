65f31ffb0db6e93dda71db2eef7950f769caf799
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-01-15 19:17:26 -0800

    test commit from new workspace

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          really cool website test
         </a>
       </header>
     </div>
